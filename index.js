765d48b5172e61115ca449d941807eed12839fc3
Guilherme Mattoso <guilhermemktfran@gmail.com>
2022-07-04 19:00:56 -0300

    projeto dockerizado com sucesso

--- index.js
+++ index.js
@@ -12,44 +12,18 @@ const conn = require("./conn/conn")
 const TasksRouter = require("./routes/tasks.routes");
 const NodeHog = require("nodehog");
 const app = express();
+const db_url = process.env.MONGODB_URI;
 
 app.use(express.json());
 app.use(cors());
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const serverStatus = () => {
-  return {
-    state: "up",
-    dbState: mongoose.STATES[mongoose.connection.readyState],
-  };
-};
-
-const db_url = process.env.MONGODB_URI;
-conn(db_url);
 mongoose.Promise = global.Promise;
-app.get("/health", (res, req) => {
-  let healthResult = serverStatus();
-  if (mongoose.connection.readyState == 0) {
-    req.statusCode = 500;
-    req.send("down");
-  } else {
-    req.json(healthResult);
-  }
-});
+conn(db_url);
 
-app.get(
-  "/stress/:elemento/tempostress/:tempoStress/tempofolga/:tempoFolga/ciclos/:ciclos",
-  (req, res) => {
-    const elemento = req.params.elemento;
-    const tempoStress = req.params.tempoStress * 1000;
-    const tempoFolga = req.params.tempoFolga * 1000;
-    const ciclos = req.params.ciclos;
-    new NodeHog(elemento, tempoStress, tempoFolga, ciclos).start();
-    res.send("OK");
-  }
-);
 
 app.use("/tasks", TasksRouter);
 
